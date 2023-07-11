f9dd2892dbce46b271e2ba927a0ec355cf3646ef
p_len <p_lenartowicz@hotmail.com>
2023-07-11 14:44:40 +0200

    Kontrola

--- src/plikTestowy.java
+++ src/plikTestowy.java
@@ -4,13 +4,15 @@ import java.io.InputStreamReader;
 public class plikTestowy {
     public static void main (String[] args) throws java.lang.Exception
     {
-        double a, b, pole;
+        double przedzial_poczatkowy1, przedzial_koncowy1;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Program oblicza pole prostokąta.");
-        a = Double.parseDouble(br.readLine());
-        b = Double.parseDouble(br.readLine());
-        pole = a*b;
-        System.out.println("Pole prostokąta o boku a = " + a + "o boku b = " + b);
-                System.out.println(" wynosi " + pole + ".");
+
+        System.out.println("Podaj liczbę poczatkową przedziału ilości termitów znajdującychb się w pierwszej ekipie!");
+        przedzial_poczatkowy1 = Double.parseDouble(br.readLine());
+        System.out.println("Podaj liczbę końcowa przedziału ilości termitów znajdującychb się w pierwszej ekipie!");
+        przedzial_koncowy1 = Double.parseDouble(br.readLine());
+
+        System.out.println("Liczba początkowa przedziału w pierwszej grupie wynosi: " + przedzial_poczatkowy1);
+        System.out.println("Liczba końcowa przedziału w pierwszej grupie wynosi: " + przedzial_koncowy1);
     }
 }
