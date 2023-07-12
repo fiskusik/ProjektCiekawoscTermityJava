945e0fb388ef93509804951d804992a818d712c1
Paweł Lenartowicz <p_lenartowicz@hotmail.com>
2023-07-12 09:43:01 +0200

    Test (MAX, MIN)

--- src/plikTestowy.java
+++ src/plikTestowy.java
@@ -2,17 +2,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class plikTestowy {
-    public static void main (String[] args) throws java.lang.Exception
+    public static void main (String[] args)
     {
-        double przedzial_poczatkowy1, przedzial_koncowy1;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double dana_a = 123456789;
+        double dana_b = 987654345;
 
-        System.out.println("Podaj liczbę poczatkową przedziału ilości termitów znajdującychb się w pierwszej ekipie!");
-        przedzial_poczatkowy1 = Double.parseDouble(br.readLine());
-        System.out.println("Podaj liczbę końcowa przedziału ilości termitów znajdującychb się w pierwszej ekipie!");
-        przedzial_koncowy1 = Double.parseDouble(br.readLine());
-
-        System.out.println("Liczba początkowa przedziału w pierwszej grupie wynosi: " + przedzial_poczatkowy1);
-        System.out.println("Liczba końcowa przedziału w pierwszej grupie wynosi: " + przedzial_koncowy1);
+        System.out.println("Wynik ->  max: " + Math.max(dana_a, dana_b) + ".");
+        System.out.println("Wynik ->  min: " + Math.min(dana_a, dana_b) + ".");
     }
 }
