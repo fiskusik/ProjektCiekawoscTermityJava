4f91c4c38a8a910ab9582fa35cb72e7906f62898
Paweł Lenartowicz <p_lenartowicz@hotmail.com>
2023-07-12 09:43:01 +0200

    Następna rzecz zrobiona (:o)

--- src/plikTestowy.java
+++ src/plikTestowy.java
@@ -4,10 +4,6 @@ import java.io.InputStreamReader;
 public class plikTestowy {
     public static void main (String[] args)
     {
-        double dana_a = 123456789;
-        double dana_b = 987654345;
 
-        System.out.println("Wynik ->  max: " + Math.max(dana_a, dana_b) + ".");
-        System.out.println("Wynik ->  min: " + Math.min(dana_a, dana_b) + ".");
     }
 }
